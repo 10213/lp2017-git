aa4d880185ea762618d05c15c00f6e4e2d11bba3
Alda Carvalho <alda.carvalho163@gmail.com.com>
2017-05-03 16:20:40 +0100

    55

---
+++ Aula10_pratica/src/aula10/MyClass.java
@@ -0,0 +1,16 @@
+package aula10;
+
+public class MyClass {
+
+	public MyClass() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	int fvvfdsx;
+
+}
---
+++ Aula10_pratica/src/aula10/My_file_local_side.java
@@ -0,0 +1,14 @@
+package aula10;
+
+public class My_file_local_side {
+
+	public My_file_local_side() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
